d624c7653c28b6d75a634e4bdddb7f60785dcd6b
Noah Horwitz and Tyler Yates <noah@>
2019-10-22 11:39:11 -0700

    initialize & add cards & explore toggling text

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  // EXAMPLES - START
+  // $(".clickable").click(function() {
+  //
+  //   $("#walrus-showing").slideToggle();
+  //   $("#walrus-hidden").slideToggle();
+  //
+  //   $("#right").removeClass("noBackground");
+  //   $("#right").addClass("green-background");
+  //
+  //   $(this).toggleClass("highlight")
+  // });
+  // EXAMPLES - END
+  $('.card').click(function(){
+    $('.textA').slideToggle();
+    $('.textB').slideToggle();
+
+  });
+});
