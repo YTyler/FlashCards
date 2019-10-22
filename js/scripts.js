ec6c315908425bdc4d8b3466ecdd0351aebd26d9
Noah Horwitz and Tyler Yates <tyler@>
2019-10-22 14:24:12 -0700

    beautify

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,10 @@
 $(document).ready(function() {
-  // EXAMPLES - START
-  // $(".clickable").click(function() {
-  //
-  //   $("#walrus-showing").slideToggle();
-  //   $("#walrus-hidden").slideToggle();
-  //
-  //   $("#right").removeClass("noBackground");
-  //   $("#right").addClass("green-background");
-  //
-  //   $(this).toggleClass("highlight")
-  // });
-  // EXAMPLES - END
-
   $('.card').click(function(){
     $(this).children().children('.textA').slideToggle();
     $(this).children().children('.textB').slideToggle();
     $(this).children().toggleClass("flipped");
   });
-
-  $('.memorized').click(function(){
+    $('.memorized').click(function(){
     $(this).parent().parent().fadeToggle();
   });
 });
